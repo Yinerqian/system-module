f97361d05f2ae8c3b867530955d31ee00bf81f77
zhangahong <18952062578@163.com>
2022-07-27 09:50:38 +0800

    物料时间格式化

--- cii-engine-entity/src/main/java/com/celi/system/cii/engine/entity/Material.java
+++ cii-engine-entity/src/main/java/com/celi/system/cii/engine/entity/Material.java
@@ -1,6 +1,7 @@
 package com.celi.system.cii.engine.entity;
 
 import com.celi.cii.base.entity.BaseCreateBy;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -31,11 +32,13 @@ public class Material extends BaseCreateBy {
     /**
      * 物料生产开始时间
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date startTime;
 
     /**
      * 物料生产结束时间
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date endTime;
 
     /**
