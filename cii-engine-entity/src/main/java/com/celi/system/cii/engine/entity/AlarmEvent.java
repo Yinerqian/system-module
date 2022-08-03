02da2654726b8d073670fabf450edf85f253f326
changaowen <changaowen1130@163.com>
2022-08-03 10:18:20 +0800

    告警恢复时间添加JsonFormat

--- cii-engine-entity/src/main/java/com/celi/system/cii/engine/entity/AlarmEvent.java
+++ cii-engine-entity/src/main/java/com/celi/system/cii/engine/entity/AlarmEvent.java
@@ -113,6 +113,7 @@ public class AlarmEvent extends BaseCreateBy {
     /**
      * 恢复时间
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date recoveryDate;
 
     @JsonProperty("alarmStatusName")
