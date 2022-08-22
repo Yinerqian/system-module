693f7f390fa5447f7f5d3e1afa78cd13f485a955
励世邦 <irelia0224@gmail.com>
2022-08-22 10:36:34 +0800

    告警规则实体类

---
+++ cii-engine-entity/src/main/java/com/celi/system/cii/engine/entity/AlarmRule.java
@@ -0,0 +1,74 @@
+package com.celi.system.cii.engine.entity;
+
+import com.celi.cii.base.entity.BaseCreateBy;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * 告警规则
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AlarmRule extends BaseCreateBy {
+    /**
+     * 告警规则id
+     */
+    private String ruleId;
+
+    /**
+     * 规则分组id
+     */
+    private String groupId;
+
+    /**
+     * 告警规则名称
+     */
+    private String ruleName;
+
+    /**
+     * 规则等级id
+     */
+    private String ruleLevelId;
+
+    /**
+     * 规则模式 1-基础模式 2-高级模式
+     */
+    private Integer ruleMode;
+
+    /**
+     * 是否开启防抖 1-开启 0-不开启
+     */
+    private Integer antiShakeFlag;
+
+    /**
+     * 防抖json
+     */
+    private String antiShakeJson;
+
+    /**
+     * 基础模式json
+     */
+    private String baseModeJson;
+
+    /**
+     * 高级模式json
+     */
+    private String advanceModeJson;
+
+    /**
+     * 执行动作json
+     */
+    private String actionJson;
+
+    /**
+     * 备注
+     */
+    private String remark;
+
+}
