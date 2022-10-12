cea759dd78ba9d088b9b1368cf9e7df05d9928c2
changaowen <changaowen1130@163.com>
2022-10-12 14:08:06 +0800

    cii-permission修改项目结构

---
+++ cii-permission/src/main/java/com/celi/system/permission/enums/SysUserStatusEnum.java
@@ -0,0 +1,43 @@
+package com.celi.system.permission.enums;
+
+import com.celi.cii.base.entity.BaseEnum;
+import lombok.Getter;
+
+@Getter
+public enum SysUserStatusEnum implements BaseEnum {
+
+    ENABLE("0", "启用"),
+    DISABLED("1", "禁用")
+    ;
+
+    private String code;
+    private String desc;
+
+    SysUserStatusEnum(String code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String getTitle() {
+        return desc;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+}
