9242623ecb889954ce2e8fb37efd8890af376257
yinxining <yinerqian@gmail.com>
2022-10-26 16:59:02 +0800

    FishBoneData添加序列化

--- cii-engine-entity/src/main/java/com/celi/system/cii/engine/entity/FishBoneData.java
+++ cii-engine-entity/src/main/java/com/celi/system/cii/engine/entity/FishBoneData.java
@@ -2,10 +2,11 @@ package com.celi.system.cii.engine.entity;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
-public class FishBoneData {
+public class FishBoneData implements Serializable {
 
     private String label;
 
